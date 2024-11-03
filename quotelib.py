2cd23ed7a231de542ef77445e8a4f0a1e481cc56
kf5bmz <bvl2clerk@gmail.com>
2024-11-03 11:45:09 -0600

    Split quote out

---
+++ quotelib.py
@@ -0,0 +1,7 @@
+# Quotes for the weekly ward bulletin / program
+# Main Program Writer will import this script and read the current quote and author
+#
+# Sources should be limited to General Authorities and General Officers of the Church of Jesus Christ of Latter-Day Saints
+
+text = "Tithing is a principle that is fundamental to the personal happiness and well-being of the Church members worldwide, both rich and poor. Tithing is a principle of sacrifice and a key to the opening of the windows of heaven."
+author = "James E. Faust"
