9ea6cc07ecf701dc7333a40931463644120183f2
maksokami <maksokami@gmail.com>
2025-02-04 20:41:47 -0600

    Update quotelib.py

--- quotelib.py
+++ quotelib.py
@@ -3,5 +3,5 @@
 #
 # Sources should be limited to General Authorities and General Officers of the Church of Jesus Christ of Latter-Day Saints
 
-text = "Tithing is a principle that is fundamental to the personal happiness and well-being of the Church members worldwide, both rich and poor. Tithing is a principle of sacrifice and a key to the opening of the windows of heaven."
-author = "James E. Faust"
+text = "Christ not only calms the physical seas, but He can also calm the seas within our minds and souls. […] It is not the velocity of the storm that makes our Heavenly Father and Jesus Christ willing to help us. It is our faith in Christ’s calming power and our hastening to call for His help that allows Him to calm our hearts during life’s storms."
+author = "Whitney Hinckley"
