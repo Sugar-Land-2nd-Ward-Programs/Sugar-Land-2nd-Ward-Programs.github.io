103ce45a80411d1504baa83ce63398db21132082
maksokami <maksokami@gmail.com>
2025-02-06 17:17:32 -0600

    Update quotelib.py

--- quotelib.py
+++ quotelib.py
@@ -3,5 +3,5 @@
 #
 # Sources should be limited to General Authorities and General Officers of the Church of Jesus Christ of Latter-Day Saints
 
-text = "Christ not only calms the physical seas, but He can also calm the seas within our minds and souls. […] It is not the velocity of the storm that makes our Heavenly Father and Jesus Christ willing to help us. It is our faith in Christ’s calming power and our hastening to call for His help that allows Him to calm our hearts during life’s storms."
-author = "Whitney Hinckley"
+text = "How do we access the quiet miracle that the Lord works as He transforms us, His children, into worthy heirs of the kingdom of God? […] I believe it comes as we yield to the enticings of the Spirit, put off the natural man, and are filled with the love of God. […] All mankind — that includes me, that includes you — we can each have part in the Atonement, the greatest of all God’s miracles."
+author = "Sydney S. Reynolds"
