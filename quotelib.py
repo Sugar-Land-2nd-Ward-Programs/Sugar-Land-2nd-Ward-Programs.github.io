ff44245f1b37b7fc09c9e1267ba3e19895bc4b76
maksokami <maksokami@gmail.com>
2025-02-23 20:25:05 -0600

    Update quotelib.py

--- quotelib.py
+++ quotelib.py
@@ -3,5 +3,5 @@
 #
 # Sources should be limited to General Authorities and General Officers of the Church of Jesus Christ of Latter-Day Saints
 
-text = "Christ, the Prince of Peace, teaches us a better way. ...  When we forgive others, it frees us to choose how we will live our own lives. Forgiveness means that problems of the past no longer dictate our destinies, and we can focus on the future with God’s love in our hearts."
-author = "David E. Sorensen"
+text = "It is important to recognize that God’s ultimate purpose is our progress. His desire is that we continue \“from grace to grace, until [we receive] a fulness\” of all He can give. That requires more than simply being nice or feeling spiritual. It requires faith in Jesus Christ, repentance, baptism of water and of the Spirit, and enduring in faith to the end. One cannot fully achieve this in isolation, so a major reason the Lord has a church is to create a community of Saints that will sustain one another in the \“strait and narrow path which leads to eternal life.\""
+author = "D. Todd Christofferson"
