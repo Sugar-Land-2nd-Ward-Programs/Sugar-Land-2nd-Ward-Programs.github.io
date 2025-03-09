f4e6237a92c9bdd607f3aab167b26fc26603547a
maksokami <maksokami@gmail.com>
2025-03-08 20:33:08 -0600

    Update quotelib.py

--- quotelib.py
+++ quotelib.py
@@ -3,5 +3,5 @@
 #
 # Sources should be limited to General Authorities and General Officers of the Church of Jesus Christ of Latter-Day Saints
 
-text = "It is important to recognize that God’s ultimate purpose is our progress. His desire is that we continue \“from grace to grace, until [we receive] a fulness\” of all He can give. That requires more than simply being nice or feeling spiritual. It requires faith in Jesus Christ, repentance, baptism of water and of the Spirit, and enduring in faith to the end. One cannot fully achieve this in isolation, so a major reason the Lord has a church is to create a community of Saints that will sustain one another in the \“strait and narrow path which leads to eternal life.\""
-author = "D. Todd Christofferson"
+text = "The most important event in time and eternity is the Atonement of Jesus Christ. He who accomplished the Atonement has given us the ordinance of the sacrament to help us not only remember but also claim the blessings of this supreme act of grace."
+author = "James J. Hamula"
